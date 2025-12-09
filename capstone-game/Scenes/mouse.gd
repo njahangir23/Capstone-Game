6c884335fdcc4cb77c166fc045b2cee37e7554f6
njahangir23 <njahangir@fordham.edu>
2025-12-09 00:19:53 -0500

    updating

--- capstone-game/Scenes/mouse.gd
+++ capstone-game/Scenes/mouse.gd
@@ -1,20 +1,24 @@
-extends Node2D
+extends Node2D   # or CharacterBody2D if using physics
 
-const SPEED = 60
+# Movement speed
+const SPEED = 80.0
 
+# Direction: 1 = right, -1 = left
 var direction = 1
 
+# Node references
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite = $AnimatedSprite2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Check collisions with walls
 	if ray_cast_right.is_colliding():
 		direction = -1
 		animated_sprite.flip_h = true
-	if ray_cast_left.is_colliding():
+	elif ray_cast_left.is_colliding():
 		direction = 1
 		animated_sprite.flip_h = false
-	
+
+	# Move the rat
 	position.x += direction * SPEED * delta
