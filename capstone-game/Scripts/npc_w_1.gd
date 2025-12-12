eee84c3bd93e8a1a6555481ce0850d91aa6922b5
mbravorubio <mbravo11@fordham.edu>
2025-12-12 02:54:54 -0500

    npc talk

--- capstone-game/Scripts/npc_w_1.gd
+++ capstone-game/Scripts/npc_w_1.gd
@@ -15,7 +15,6 @@ var player_in_range: bool = false
 
 
 func _ready() -> void:
-	# Patrol limits based on starting position
 	left_limit = global_position.x + left_offset
 	right_limit = global_position.x + right_offset
 
@@ -24,7 +23,6 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	# Simple left/right patrol
 	velocity.x = direction * speed
 	move_and_slide()
 
@@ -42,8 +40,8 @@ func _face_direction(dir: int) -> void:
 
 
 func _process(delta: float) -> void:
-	# Player presses E/Enter/Space (ui_accept) while in range
 	if player_in_range and Input.is_action_just_pressed("ui_accept"):
+		print("NPC: ui_accept pressed while player in range")  # DEBUG
 		if speech_bubble:
 			speech_bubble.set_text(
 				"Get out, [color=red]of my way I'm getting my steps in[/color]",
@@ -51,13 +49,17 @@ func _process(delta: float) -> void:
 			)
 
 
-func _on_Area2D_body_entered(body: Node) -> void:
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("NPC Area2D entered by: ", body.name)  # DEBUG
 	if body.is_in_group("Player"):
+		print("NPC: Player in range")  # DEBUG
 		player_in_range = true
 
 
-func _on_Area2D_body_exited(body: Node) -> void:
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	print("NPC Area2D exited by: ", body.name)  # DEBUG
 	if body.is_in_group("Player"):
+		print("NPC: Player left range")  # DEBUG
 		player_in_range = false
 		if speech_bubble:
 			speech_bubble.visible = false
